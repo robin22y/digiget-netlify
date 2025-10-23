ff22203097f0dd3a361ac385bd33bbe06272b761
robin22y <robin22y@gmail.com>
2025-10-23 21:10:33 +0100

    Fix Firebase config and admin login

---
+++ assets/firebase-config.js
@@ -0,0 +1,9 @@
+// DigiGet L9 Firebase configuration
+window.firebaseConfig = {
+  apiKey: "AIzaSyDUBVjVH22qIBvHeknfeGXteQuT6jikvAA",
+  authDomain: "digiget-l9.firebaseapp.com",
+  projectId: "digiget-l9",
+  storageBucket: "digiget-l9.firebasestorage.app",
+  messagingSenderId: "66838869613",
+  appId: "1:66838869613:web:51bf0664d55a3c164e3164"
+};
